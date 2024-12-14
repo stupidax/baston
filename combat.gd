5b3de671d0741d083dcee810007b0329c20cf444
PapageiSquirrel <raishinshi@hotmail.com>
2024-12-14 22:11:02 +0100

    Manage attack signal between players

--- combat.gd
+++ combat.gd
@@ -1,10 +1,15 @@
 extends Node
 
+@onready var p1_node = get_parent().get_node("Player");
+@onready var p2_node = get_parent().get_node("Player2");
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	p1_node.connect("on_attack", self.handleAttack.bind(self, p2_node));
+	p2_node.connect("on_attack", self.handleAttack.bind(self, p1_node));
 
+func handleAttack(attack_type, _param, player_node) -> void:
+	player_node.attack_received(attack_type);
 
 func fight() -> void:
 	pass
--- player/player.gd
+++ player/player.gd
@@ -28,26 +28,32 @@ class Player:
 	const MAX_CHARGE = 30;
 	
 	var life;
-	var sprite
+	var sprite;
+	var attack_action;
+	var block_action;
 	var current_animation;
 	var charge_frames;
 	var is_charging;
 	var current_attack_type;
 	
-	signal on_attack(type);
+	var on_attack;
 	
-	func _init(life_param, sprite_param):
+	func _init(life_param, sprite_param, attack_action_param, block_action_param, attack_signal):
 		self.life = life_param;
 		self.sprite = sprite_param;
+		self.attack_action = attack_action_param;
+		self.block_action = block_action_param;
 		self.set_current_animation(Animations.IDLE);
 		self.charge_frames = 0;
 		self.is_charging = false;
+		
+		on_attack = attack_signal;
 	
 	func set_current_animation(new_animation):
 		self.current_animation = new_animation;
 		self.sprite.play(new_animation);
 	func is_action_allowed() -> bool:
-		return self.current_animation == Animations.IDLE || self.current_animation == Animations.BLOCK;
+		return self.current_animation == Animations.IDLE || self.current_animation == Animations.BLOCK || self.current_animation == Animations.HIT;
 	
 	func compute_charge():
 		if self.is_charging:
@@ -68,63 +74,75 @@ class Player:
 		return self.charge_frames == MAX_CHARGE;
 	
 	func compute_received_attack(attack_received) -> void:
+		var is_hit = false;
 		if attack_received == attack_type.IMPARABLE:
 			self.life -= IMPARABLE_DAMAGE;
+			is_hit = true;
 		elif self.current_animation != Animations.PARADE && self.sprite.frame <= 1:
 			# TODO: handle parade consequences
 			self.life;
 		elif self.current_animation != Animations.BLOCK:
 			if attack_received == attack_type.CHARGED:
 				self.life -= STRONG_DAMAGE;
+				is_hit = true;
 			else:
 				self.life -= SIMPLE_DAMAGE;
+				is_hit = true;
+		
+		if self.is_dead():
+			self.set_current_animation(Animations.DEAD);
+		elif is_hit:
+			self.set_current_animation(Animations.HIT);
 				
 	func is_dead() -> bool:
 		return self.life < 1;
 
-@export var playerLife: int = Player.DEFAULT_LIFE;
+@export var player_life: int = Player.DEFAULT_LIFE;
+@export var player_attack: String;
+@export var player_block: String;
+
+func attack_received(attack_type) -> void:
+	player.compute_received_attack(attack_type);
+
+signal on_attack();
 
 var player;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player = Player.new(playerLife, $AnimatedSprite2D);
+	player = Player.new(player_life, $AnimatedSprite2D, player_attack, player_block, on_attack);
 	$AnimatedSprite2D.frame_changed.connect(_animation_progress.bind($AnimatedSprite2D));
-	$AnimatedSprite2D.animation_finished.connect(_animation_end);
+	$AnimatedSprite2D.animation_finished.connect(_animation_end.bind($AnimatedSprite2D));
 
 func _animation_progress(sprite) -> void:
-	if sprite.animation == Animations.ATTACK && sprite.is_playing() && sprite.frame == 4 && Input.is_action_pressed(Animations.ATTACK):
+	if sprite.animation == Animations.ATTACK && sprite.is_playing() && sprite.frame == 4 && Input.is_action_pressed(player.attack_action):
 		sprite.pause();
 		player.is_charging = true;
 	if sprite.animation == Animations.ATTACK && sprite.is_playing() && sprite.frame == 7:
 		player.on_attack.emit(player.current_attack_type);
 
-func _animation_end() -> void:
-	player.set_current_animation(Animations.IDLE);
-
-func handleAnimation(event, animation) -> void:
-	if event.is_action_pressed(animation):
-		player.set_current_animation(animation);
+func _animation_end(sprite) -> void:
+	if sprite.animation != Animations.DEAD && sprite.animation != Animations.IDLE:
+		player.set_current_animation(Animations.IDLE);
 
 func _input(event) -> void:
-	if event.is_action_released(Animations.ATTACK):
+	if event.is_action_released(player.attack_action):
 		player.current_attack_type = from_frame_to_attack_type(player.charge_frames);
 		$AnimatedSprite2D.play();
 		player.is_charging = false;
 		player.reset_charge_frames();
 		
-	if event.is_action_released(Animations.BLOCK):
+	if event.is_action_released(player.block_action):
 		player.set_current_animation(Animations.IDLE);
 	
 	if !player.is_action_allowed():
 		return;
 	
-	handleAnimation(event, Animations.ATTACK);
-	handleAnimation(event, Animations.COUNTER);
-	handleAnimation(event, Animations.BLOCK);
+	if event.is_action_pressed(player.attack_action):
+		player.set_current_animation(Animations.ATTACK);
+	if event.is_action_pressed(player.block_action):
+		player.set_current_animation(Animations.BLOCK);
+	# TODO: how is handled counter ?
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player.is_dead():
-		player.set_current_animation(Animations.DEAD);
-		
 	player.compute_charge();
