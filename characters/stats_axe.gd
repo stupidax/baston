190cf5b1ceb1fe981af27f9dc11027b8d0f199c3
PapageiSquirrel <perrosquirrel@gmail.com>
2025-02-22 14:21:40 +0100

    Fix bugs with new animation_player

--- characters/stats_axe.gd
+++ characters/stats_axe.gd
@@ -16,35 +16,29 @@ extends Node
 
 @export_group("Frames data")
 @export var charge_frames = {
-	Constants.attack_type.MEDIUM: 4,
-	Constants.attack_type.STRONG: 12,
-	Constants.key_word.CAP: 15
+	Constants.attack_type.MEDIUM: 90,
+	Constants.attack_type.STRONG: 94,
+	Constants.key_word.CAP: 96
 };
 @export var start_up_frames = {
-	Constants.attack_type.LIGHT: 3,
-	Constants.attack_type.MEDIUM: 5,
-	Constants.attack_type.STRONG: 7,
-	Constants.key_word.PARADE: 0
+	Constants.attack_type.LIGHT: 20,
+	Constants.attack_type.MEDIUM: 42,
+	Constants.attack_type.STRONG: 64,
+	Constants.key_word.PARADE: 101
 };
 @export var active_frames = {
-	Constants.attack_type.LIGHT: 3,
-	Constants.attack_type.MEDIUM: 3,
-	Constants.attack_type.STRONG: 4,
-	Constants.key_word.PARADE: 2
-};
-@export var recovery_frames = {
-	Constants.attack_type.LIGHT: 5,
-	Constants.attack_type.MEDIUM: 10,
-	Constants.attack_type.STRONG: 15,
-	Constants.key_word.PARADE: 0
+	Constants.attack_type.LIGHT: 21,
+	Constants.attack_type.MEDIUM: 44,
+	Constants.attack_type.STRONG: 65,
+	Constants.key_word.PARADE: 103
 };
 @export var block_stun_frames = {
-	Constants.attack_type.LIGHT: 6,
-	Constants.attack_type.MEDIUM: 12,
+	Constants.attack_type.LIGHT: 15,
+	Constants.attack_type.MEDIUM: 35,
 };
 @export var hit_stun_frames = {
 	Constants.attack_type.LIGHT: 6,
 	Constants.attack_type.MEDIUM: 9,
-	Constants.attack_type.STRONG: 0
+	Constants.attack_type.STRONG: 61
 };
-@export var counter_frames = 0;
+@export var counter_frames = 60;
